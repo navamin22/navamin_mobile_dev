07c3927b7eccf01ab621bce332d21b6dd33c5e7a
Navamin <navamin2240@gmail.com>
2021-10-21 17:02:50 +0700

    update

---
+++ lib/src/screens/login/login.dart
@@ -0,0 +1,314 @@
+// ignore: use_key_in_widget_constructors
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:navamin/src/screens/menu/pokemoninfor.dart';
+import 'package:navamin/src/screens/provider/google_sign_in.dart';
+import 'package:navamin/src/screens/register/register.dart';
+import 'package:navamin/src/widgets/appBg.dart';
+import 'package:provider/provider.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  // ignore: unused_field
+  // User _firebaseUser;
+  final userController = TextEditingController();
+  final passController = TextEditingController();
+  // ignore: unused_field
+  String _username = "";
+  // ignore: unused_field
+  String _password = "";
+  bool rememberMe = false;
+  bool _isHidden = true;
+  bool isSignIn = false;
+
+  void _toggleVisibility() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+
+  Widget _buildUsernameTF() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(left: 12),
+          child: Text(
+            'Email',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 70.0,
+          child: TextField(
+            keyboardType: TextInputType.text,
+            controller: userController,
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              hintText: 'Email',
+              hintStyle: TextStyle(fontSize: 16),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              prefixIcon: Icon(
+                Icons.mail_outline,
+                color: Colors.black,
+              ),
+              filled: true,
+              fillColor: Colors.white,
+              // contentPadding: EdgeInsets.only(top: 14.0),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPasswordTF(String hintText) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(left: 12),
+          child: Text(
+            'Password',
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 70.0,
+          child: TextField(
+            obscureText: _isHidden,
+            controller: passController,
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              hintText: 'Enter your Password',
+              hintStyle: TextStyle(fontSize: 16),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide(
+                    width: 3,
+                  )),
+              filled: true,
+              fillColor: Colors.white,
+              // contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock_outlined,
+                color: Colors.black,
+              ),
+              suffixIcon: hintText == "Password"
+                  ? IconButton(
+                      onPressed: _toggleVisibility,
+                      icon: _isHidden
+                          ? Icon(Icons.visibility_off_outlined)
+                          : Icon(Icons.visibility_outlined),
+                    )
+                  : null,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLoginBtn() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => PokemonPage(
+                    title: '',
+                  )));
+        },
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          // side: BorderSide(color: Colors.grey, width: 3)
+        ),
+        color: Colors.grey[600],
+        child: Text(
+          'เข้าสู่ระบบ',
+          style: TextStyle(
+            color: Colors.white,
+            letterSpacing: 1.5,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Neo Sans Arabie Medium',
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRegisterBtn() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 0.0),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => Register()));
+        },
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          // side: BorderSide(color: Colors.grey, width: 3)
+        ),
+        color: Colors.grey[800],
+        child: Text(
+          'สมัครสมาชิก',
+          style: TextStyle(
+            color: Colors.white,
+            letterSpacing: 1.5,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'OpenSans',
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildGoogleBtn() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 0.0),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () {
+//เปิดฟังก์ชั่นต่อ google
+          // final provider =
+          //     Provider.of<GoogleSignInProvider>(context, listen: false);
+          // provider.googleLogin();
+        },
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          // side: BorderSide(color: Colors.grey, width: 3)
+        ),
+        color: Colors.white,
+        child: Text(
+          'Google',
+          style: TextStyle(
+            color: Colors.black,
+            letterSpacing: 1.5,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'OpenSans',
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFacebookBtn() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 0.0),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () {},
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          // side: BorderSide(color: Colors.grey, width: 3)
+        ),
+        color: Colors.blue[300],
+        child: Text(
+          'Facebook',
+          style: TextStyle(
+            color: Colors.white,
+            letterSpacing: 1.5,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'OpenSans',
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Stack(
+            children: <Widget>[
+              AppBackground(),
+              Container(
+                height: double.infinity,
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40.0,
+                    vertical: 20.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 40.0,
+                      ),
+                      Text(
+                        'เข้าสู่ระบบ',
+                        style: TextStyle(
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontFamily: 'OpenSans',
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      _buildUsernameTF(),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      _buildPasswordTF('Password'),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      _buildLoginBtn(),
+                      _buildRegisterBtn(),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      _buildGoogleBtn(),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      _buildFacebookBtn(),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
